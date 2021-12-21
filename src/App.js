44229c0dbe996d0bea7b5cac277cbf565695c3f1
Assis Ferreira <ferreira.assisruny@gmail.com>
2021-12-21 08:13:10 -0100

    layout done

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ function App() {
       <header className="w-4/5 mx-auto py-3">
         <h1 className='text-white font-mono text-2xl'>React Pomodoro</h1>
       </header>
-      <section className="w-4/5 mx-auto py-3 bg-red-300">
-        <h3 className='text-white text-4xl'>25:00</h3>
+      <section className="w-4/5 mx-auto bg-red-300 rounded p-5 text-center">
+        <h3 className='text-white text-8xl font-bold'>25:00</h3>
+        <button className='mt-5 text-red-300 bg-white text-xl rounded py-2 w-48 border-2 border-white hover:bg-red-300 hover:text-white'>
+          Start
+        </button>
       </section>
     </main>
   );
